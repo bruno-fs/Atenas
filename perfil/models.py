55edb7b903b54f83d9bc836fc4aba21dd7ab0a6a
Lucas <lucas.ciconelle@gmail.com>
2021-12-21 14:40:01 -0300

    alterei o models novamente.py

--- perfil/models.py
+++ perfil/models.py
@@ -4,68 +4,65 @@ from django.utils import timezone
 
 
 # Create your models here.
-class Equipamentos(models.Model):
-    marca = models.ForeignKey('Marca', on_delete=models.CASCADE)# Tabela Marca - 1 Modelo -> Mtos Equipamentos
-                                                            #Se é Lenovo, Dell, HP etc etc
-    modelo = models.Charfield(max_length=50)
-    num_serie = models.Charfield(max_length=30)
-    categoria = models.ForeignKey('Categoria', on_delete=models.CASCADE)# Tabela Categoria - 1 Categorias -> Mtos Equipamentos
-                                                            #Se é um notebook, Torre, Servidor etc etc
-    hist_rma = models.TextField(null=True)
-    host_contrato = models.TextField(null=True)
-    especificacao = models.TextField()
-    softwares = models.ForeignKey('Softwares', on_delete=models.CASCADE)# Tabela Software - Mts Softwares -> 1 Equipamento
-                                                            #Quais softwares estão instalados no computador
 
-    ESCOLHACONDICAO = (
+class Equipamento(models.Model):
+
+       ESCOLHACONDICAO = (
         ("NOVO", "Novo"),
         ("USADO", "Usado"),
     )
+
+    marca = models.ForeignKey('Marca', on_delete=models.CASCADE)# Tabela Marca - 1 Modelo -> Mtos Equipamentos #Se é Lenovo, Dell, HP etc etc
+
+    modelo = models.Charfield(max_length=50)
+
+    num_serie = models.Charfield(max_length=30)
+
+    categoria = models.ForeignKey('Categoria', on_delete=models.CASCADE)# Tabela Categoria - 1 Categorias -> Mtos Equipamentos #Se é um notebook, Torre, Servidor etc etc
+   
+    hist_rma = models.TextField(null=True)
+
+    host_contrato = models.TextField(null=True)
+
+    especificacao = models.TextField()
+
+    locacao = models.ForeignKey('Locacao', on_delete=models.CASCADE, null=True)# Tabela Locacao # - Mtos Equipamentos -> 1 Locacao #Quais equipamentos foram alugados
+    
     condicao = models.Charfield(max_length=5, choices=ESCOLHACONDICAO) #O equipamento é NOVO ou USADO
 
-    ESCOLHASTATUS = (
-        ('DPN', 'Disponivel'),
-        ('ECT', 'Em Contrato'),
-        ('RMA', 'Manutenção'),
-        ('VND', 'Vendido'),
-        ('IDP', 'Indisponivel'),
-    )
-    status = models.Charfield(max_length=3, choices=ESCOLHASTATUS) #O equipamento pode ou não ser alugado?
-
-
-
-
+    status = models.ForeignKey('Status_Notebook', on_delete=models.CASCADE) #1 Status -> 1 Equipamento
+    
+    
 class Marca(models.Model):
+
     nome = models.Charfield(max_length=15)
 
 class Categoria(models.Model):
+
     nome = models.Charfield(max_length=15)
 
 class Software(models.Model):
+
     nome = models.Charfield(max_length=20)
+
     serial = models.TextField(null=True)
 
+    equipamento = models.ForeignKey('Equipamento', on_delete=models.CASCADE, null=True)# Tabela Software - Mts Softwares -> 1 Equipamento
+
 class Cliente(models.Model):
+
     nome = models.Charfield(max_length=50)
-    cpnj = models.Charfield(max_length=20)
-    endereco = models.Charfield(max_length=100)
-    contato = models.Charfield(max_lenght=50)
 
-class Locacao():
-    num_contrato = models.Charfield(max_length=10)
-    cliente = models.ForeignKey('Cliente', on_delete=models.CASCADE)# Tabela Cliente
-                                                            # 1 Cliente -> Mtos Locacao
-                                                        #Qual cliente está fazendo a locação
+    cpnj = models.IntegerField()
 
-    data_inicio = models.DateTimeField(default=timezone.now)
-    duração = models.Charfield(max_length=20)
-    equipamentos = models.ForeignKey('Equipamentos', on_delete=models.CASCADE)# Tabela Equipamentos
-                                                                            # - Mtos Equipamentos -> 1 Locacao
-                                                                            #Quais equipamentos foram alugados
+    endereco = models.Charfield()
 
-    adtivos = models.TextField()
-    historico = models.TextField()
-    valor = models.Charfield(max_length=10)
+    cep = models.Charfield()
+
+    contato = models.ForeignKey('Agenda', on_delete=models.CASCADE) # Mtos Contatos -> Mtos clientes
+    
+
+class Locacao(models.Model):
 
     STATUS_LOCACAO = (
         ('ABT', 'Aberto'),
@@ -73,13 +70,78 @@ class Locacao():
         ('RNV', 'Renovado'),
         )
 
+    num_contrato = models.Charfield(max_length=10)
+
+    cliente = models.ForeignKey('Cliente', on_delete=models.CASCADE)# Tabela Cliente # 1 Cliente -> Mtos Locacao #Qual cliente está fazendo a locação
+
+    data_inicio = models.DateTimeField(default=timezone.now)
+
+    duração = models.Charfield(max_length=20)   
+
+    adtivos = models.TextField(null=True)
+
+    historico = models.TextField(null=True)
+
+    valor = models.Charfield(max_length=10)
+
     status = models.Charfield(max_length=3, choices=STATUS_LOCACAO) # O equipamento pode ou não ser alugado?
 
-class Produtos(models.Model):
+
+class Agenda(models.Model):
+   
     nome = models.Charfield(max_length=50)
-    marcamodels.ForeignKey('Marca', on_delete=models.CASCADE)# Tabela Marca - 1 Modelo -> Mtos Equipamentos
-                                                            #Se é Lenovo, Dell, HP etc etc
+    
+    email = models.Charfield(max_length=50)
+    
+    telefones = models.Charfield(max_length=50)
+
+    empresa = models.ForeignKey('Cliente', on_delete=models.CASCADE)
+
+
+class Status_Notebook(models.Model):
+
+    ESCOLHA_STATUS = (
+        ('DPN', 'Disponivel'),
+        ('ECT', 'Em Contrato'),
+        ('RMA', 'Manutenção'),
+        ('VND', 'Vendido'),
+        ('IDP', 'Indisponivel'),
+    )
+
+    status = models.Charfield(max_length=3, choices=ESCOLHA_STATUS)
+
+    models.Charfield(max_length=3, choices=ESCOLHA_STATUS)
+    
+    data_inicio = models.DateTimeField(default=timezone.now)
+    
+    hist_status = Textfield()
+
+class Produto(models.Model):
+
+    nome = models.Charfield(max_length=50)
+
+    marca = models.ForeignKey('Marca', on_delete=models.CASCADE)# Tabela Marca - 1 Modelo -> Mtos Produtos #Se é Lenovo, Dell, HP etc etc
+
     quantidade = models.IntegerField()
-    categoria = models.ForeignKey('Categoria', on_delete=models.CASCADE)# Tabela Categoria
-                                                            # - 1 Categorias -> Mtos Equipamentos
-                                                            #Se é um notebook, Torre, Servidor etc etc
+
+    categoria = models.ForeignKey('Categoria', on_delete=models.CASCADE)# Tabela Categoria # - 1 Categorias -> Mtos Equipamentos #Se é um notebook, Torre, Servidor etc 
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
